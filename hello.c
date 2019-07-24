79fddb22a37ab74f5e3229df5189d0e28666107d
nigulapi <1078825892@qq.com>
2019-07-24 20:15:29 +0800

    reset rewrite

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(int arg, char *argv[] ) {
 	printf("aaa\n");
-	printf("version1.0,这才是最新的版本阿\n");
+	printf("bbb\n");
 	return 0;
 }
--- hello_4.c
+++ hello_4.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(int arg, char *argv[] ) {
 	printf("aaa\n");
-	printf("bbb\n");
+	printf("bbb\nsdfsafssd");
 	return 0;
-}
+}weedsdf asdfsdafsdafdsafdssad
