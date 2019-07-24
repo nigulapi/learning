44e1045b8bb05f2a071b6bfd4704b52a158ca1ce
nigulapi <1078825892@qq.com>
2019-07-24 11:29:35 +0800

    remodified hello

---
+++ hello_1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(int arg, char *argv[] ) {
+	printf("aaa\n");
+	printf("bb\n");
+	return 0;
+}
---
+++ hello_4.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(int arg, char *argv[] ) {
+	printf("aaa\n");
+	printf("bbb\n");
+	return 0;
+}
